371d33a4fc8b1974353256b417fc5d84874fd557
Ahmet Hasan Canbolat <ahmthsnpriv@gmail.com>
2026-07-09 18:43:17 +0300

    feat(auth): implement oauth2 flow and local token storage

--- src/main/java/com/eycyen/cloudconfig/auth/GoogleDriveManager.java
+++ src/main/java/com/eycyen/cloudconfig/auth/GoogleDriveManager.java
@@ -1,27 +1,50 @@
 package com.eycyen.cloudconfig.auth;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.DriveScopes;
 
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
 
 public class GoogleDriveManager {
+
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
+    private static final String TOKENS_DIRECTORY_PATH = "config/cloudconfig/tokens";
 
     public void getDriveService() {
         try {
-            InputStream in = GoogleDriveManager.class.getResourceAsStream("/client_secret.json");
+            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
 
+            InputStream in = GoogleDriveManager.class.getResourceAsStream("/client_secret.json");
             if (in == null) {
                 throw new RuntimeException("Resource not found: client_secret.json");
             }
-
             GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-            System.out.println("Successfully read! Client ID: " + clientSecrets.getDetails().getClientId());
-                
+            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                    HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                    .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
+                    .setAccessType("offline") 
+                    .build();
+
+            LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+            Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+
+            System.out.println("Authorization successful! Tokens saved to: " + new File(TOKENS_DIRECTORY_PATH).getAbsolutePath());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
